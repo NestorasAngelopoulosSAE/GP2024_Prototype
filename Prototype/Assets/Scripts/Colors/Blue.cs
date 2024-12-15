92eef48f7d0315018769ea22dfcdb76d5f4c7229
thanostham <13-13885@saeinstitute.edu>
2024-12-15 17:00:37 +0200

    Updated Blue
    blue gets direction to that of the camera.
    object has no gravity when blue is applied.
    
    PROBLEMS :
    object no gravity has odd interactions with playerCollider.
    
    speed does not seem to work.
    
    👍

--- Prototype/Assets/Scripts/Colors/Blue.cs
+++ Prototype/Assets/Scripts/Colors/Blue.cs
@@ -1,21 +1,32 @@
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Blue : MonoBehaviour
 {
-   [SerializeField] public float speed = 2f;
+    [SerializeField] public float speed = 20f;
+    Vector3 moveDirection;
+    Rigidbody RB;
+      
     void Start()
     {
-        Debug.Log($"{gameObject.name} is Blue!");   
-    }
+        Debug.Log($"{gameObject.name} is Blue!");
+        moveDirection = - (Camera.main.transform.position - transform.position);
+        RB = GetComponent<Rigidbody>();
+        RB.useGravity = false;     
+    } 
 
     public void OnDisable()
     {
         Debug.Log($"{gameObject.name} is no longer Blue!");
+        RB.useGravity = true;
     }
 
     void Update()
-    {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    {             
+        transform.position += moveDirection.normalized * Time.deltaTime;
     }
 }

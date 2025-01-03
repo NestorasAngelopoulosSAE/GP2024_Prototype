2d162d25905699f06309d208f7d615296578c0e2
Nestoras Angelopoulos <13-13877@saeinstitute.edu>
2025-01-03 21:11:18 +0200

    Aesthetic changes to Main Menu
    Made a logo that can change color.
    
    Changed all main menu buttons to have a different brushstroke and color on hover.
    
    Set "Permanent Marker" from Google Fonts as our UI font.

--- Prototype/Assets/Scripts/MainMenuManager.cs
+++ Prototype/Assets/Scripts/MainMenuManager.cs
@@ -1,9 +1,18 @@
-using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public Image Logo;
+
+    private void Start()
+    {
+        // Demonstration of the new logo: (you can remove this or use this feature differently if you like)
+        // picks the color of the logo randomly between red, green and blue.
+        Logo.color = new Color[] { Color.red, Color.green, Color.blue }[Random.Range(0, 3)];
+    }
+
+    // Nestoras: these aren't necessary since you're changing the ui through the button events.
     public GameObject MainButtonContainer;
     public GameObject SettingsButtonContainer;
     public GameObject AudioSettingsContainer;
@@ -18,7 +27,6 @@ public class MainMenuManager : MonoBehaviour
         SettingsButtonContainer.SetActive(true);
 
     }
-
     public void AudioSettings()
     {
         SettingsButtonContainer.SetActive(false);

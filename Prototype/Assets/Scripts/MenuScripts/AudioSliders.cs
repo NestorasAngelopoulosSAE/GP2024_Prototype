726ee4e3e00169df464b9bf3dc3fe76dd0d22264
thanostham <13-13885@saeinstitute.edu>
2025-01-07 22:31:30 +0200

    PlayerPrefs Audio slider saving
    PlayerPrefs audio

--- Prototype/Assets/Scripts/MenuScripts/AudioSliders.cs
+++ Prototype/Assets/Scripts/MenuScripts/AudioSliders.cs
@@ -2,24 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioSliders : MonoBehaviour
 {
 
     public AudioMixer masterMixer;
+    public Slider SFXSlider;
+    public Slider MasterSlider;
+    public Slider MusicSlider;
 
     public void SetSfxLvl(float sfxLvl)
     {
         masterMixer.SetFloat("SFXVolume", sfxLvl);
+        PlayerPrefs.SetFloat("sfxVolume", sfxLvl);
     }
    
     public void MusicLvl(float musicLvl)
     {
         masterMixer.SetFloat("MusicVolume", musicLvl);
+        PlayerPrefs.SetFloat("MusicVolume", musicLvl);
     }
 
     public void MasterLvl(float masterLvl)
     {
         masterMixer.SetFloat("MasterVolume", masterLvl);
+        PlayerPrefs.SetFloat("MasterVolume", masterLvl);
+    }
+
+    public void Start()
+    {   // Saving values
+
+        //SFX
+        if (PlayerPrefs.HasKey("sfxVolume"))
+        {
+            // set slider to player prefs value 
+            SFXSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+            // set mixer volume to player prefs value 
+            masterMixer.SetFloat("sfxVolume", SFXSlider.value);            
+        }
+
+        if (PlayerPrefs.HasKey("MasterVolume"))
+        {
+            // set slider to player prefs value 
+            MasterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+            // set mixer volume to player prefs value 
+            masterMixer.SetFloat("MasterVolume", MasterSlider.value);
+        }
+
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            // set slider to player prefs value 
+            MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+            // set mixer volume to player prefs value 
+            masterMixer.SetFloat("MusicVolume", MusicSlider.value);
+        }
     }
 }

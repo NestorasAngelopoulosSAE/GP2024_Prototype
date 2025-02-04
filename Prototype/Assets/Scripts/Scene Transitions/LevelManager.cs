89a3f423b9754c67dfed830067293abcad91a054
Nestoras Angelopoulos <nestorasangelopoulos@gmail.com>
2025-02-04 18:31:07 +0200

    Added a wipe effect to the scene transitions

--- Prototype/Assets/Scripts/Scene Transitions/LevelManager.cs
+++ Prototype/Assets/Scripts/Scene Transitions/LevelManager.cs
@@ -1,54 +1,60 @@
 /// <summary>
 /// Nestoras Angelopoulos 2025
 /// 
-/// Interface to handle loading levels.
+/// Interface to handle loading levels using the wipe transition.
 /// </summary>
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Collections;
+
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class LevelManager : MonoBehaviour
 {
-    void ResetTimeScale()
-    {
-        Time.timeScale = 1f;
-    }
+    public Material WipeMaterial;
+    readonly int _circleSizeId = Shader.PropertyToID("_Circle_Size");
 
-    public void LoadMainMenu()
-    {
-        SceneManager.LoadScene(0);
-        ResetTimeScale();
-    }
+    float wipeSpeed = 5f;
+    float waitTime = 0.5f; // Time to to hold the black screen for.
 
-    public void Reload()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        ResetTimeScale();
-    }
+    bool transitionStarted;
+    bool transitionComplete;
 
-    public void Load(string levelName)
-    {
-        Load(SceneManager.GetSceneByName(levelName).buildIndex);
-    }
+    private void Awake() => WipeMaterial.SetFloat(_circleSizeId, 0f);
 
-    public void Load(int level)
+    private void Update()
     {
+        if (Time.timeSinceLevelLoad <= waitTime / 2) return;
+
+        float size = WipeMaterial.GetFloat(_circleSizeId);
+
+        if (!transitionStarted) WipeMaterial.SetFloat(_circleSizeId, Mathf.MoveTowards(size, 1.5f, wipeSpeed * Time.unscaledDeltaTime));
+        else
+        {
+            WipeMaterial.SetFloat(_circleSizeId, Mathf.MoveTowards(size, 0, wipeSpeed * Time.unscaledDeltaTime));
+            if (size <= 0f) transitionComplete = true;
+        }
+    }
+    
+    public void Reload() => Load(SceneManager.GetActiveScene().buildIndex);
+    public void LoadMainMenu() => Load(0);
+    public void LoadNext() => Load(SceneManager.GetActiveScene().buildIndex + 1);
+    public void LoadPrevious() => Load(SceneManager.GetActiveScene().buildIndex - 1);
+    public void Load(string levelName) => Load(SceneManager.GetSceneByName(levelName).buildIndex);
+    public void Load(int level) => StartCoroutine(Transition(level));
+
+    IEnumerator Transition(int level)
+    {
+        transitionStarted = true;
+
+        while (!transitionComplete) yield return null;
+        yield return new WaitForSecondsRealtime(waitTime / 2);
+
         SceneManager.LoadScene(level);
-        ResetTimeScale();
-    }
-
-    public void LoadNext()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        ResetTimeScale();
-    }
-
-    public void LoadPrevious()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        ResetTimeScale();
+        Time.timeScale = 1f;
     }
 
     public void Quit()

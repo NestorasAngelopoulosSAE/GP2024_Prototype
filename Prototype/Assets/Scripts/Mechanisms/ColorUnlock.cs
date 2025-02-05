a0d7cc2a795930e6656f344945712e24dc09e712
NestorasAngelopoulosSAE <13-13877@saeinstitute.edu>
2025-02-05 14:13:04 +0200

    Color Unlock Scene in the Green level

--- Prototype/Assets/Scripts/Mechanisms/ColorUnlock.cs
+++ Prototype/Assets/Scripts/Mechanisms/ColorUnlock.cs
@@ -4,28 +4,45 @@
 /// Used on trigger objects enable the unlocking of new colors.
 /// </summary>
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ColorUnlock : MonoBehaviour
 {
     ColorManager colorManager;
     public int IndexOfColorToUnlock;
+    [SerializeField] Light MainLight;
+    [SerializeField] float lightTransitionSpeed = 1;
+    float targetIntensity;
+
+    public UnityEvent onUnlock;
 
     private void Start()
     {
         colorManager = GameObject.FindWithTag("Gameplay Manager").GetComponent<ColorManager>();
+        targetIntensity = MainLight.intensity;
+        MainLight.intensity = 0.2f;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" &! colorManager.GameplayColors[IndexOfColorToUnlock].unlocked)
         {
             // Unlock color.
             colorManager.GameplayColors[IndexOfColorToUnlock].unlocked = true;
             // Animate brush to switch to the new color.
             colorManager.selectedColor = IndexOfColorToUnlock;
             colorManager.brushAnimator.SetTrigger("Change");
-            // Delete the trigger object.
-            Destroy(gameObject);
+            onUnlock.Invoke();
         }
+    }
+
+    private void Update()
+    {
+        if (colorManager.GameplayColors[IndexOfColorToUnlock].unlocked)
+        {
+            MainLight.intensity = Mathf.MoveTowards(MainLight.intensity, targetIntensity, lightTransitionSpeed * Time.deltaTime);
+        }
+
+        if (MainLight.intensity == targetIntensity) Destroy(gameObject);
     }
 }
